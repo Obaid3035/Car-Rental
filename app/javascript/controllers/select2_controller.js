478ed6d2224a6c51c4d14856f8581ea0f03bfe12
Obaid <obaid3035@gmail.com>
2020-08-30 15:45:23 +0500

    Big Fix

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -1,12 +1,11 @@
 import { Controller } from "stimulus"
 import $ from 'jquery'
-import 'select2'
-import 'select2/dist/css/select2.css'
 
 export default class extends Controller {
   connect() {
   console.log("HELLO")
     $('#car_manufacturer_id').select2({
+      theme: "classic"
     });
     $('#booking_car_id').select2({
       theme: "classic"
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
+
+import 'select2'
+import 'select2/dist/css/select2.css'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
