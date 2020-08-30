5273ab2239b9430efd1a332289bc8039b646bad6
Obaid <obaid3035@gmail.com>
2020-08-30 17:32:53 +0500

    Bug Fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import 'bootstrap';
-
 import 'select2'
 import 'select2/dist/css/select2.css'
+import 'bootstrap';
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
